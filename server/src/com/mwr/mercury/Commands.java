e2db756f1066ba108d89fed7876c25aeb879de31
Mathy Vanhoef <vanhoefm@gmail.com>
2012-04-19 02:03:29 +0200

    Added required permission of services
    Now displays required permission of Services and allows you to filter on them.

--- server/src/com/mwr/mercury/Commands.java
+++ server/src/com/mwr/mercury/Commands.java
@@ -631,8 +631,9 @@ public class Commands
 			{
 				// TODO Auto-generated method stub
 				
-				//Assign filter if one came in the arguments
+				//Assign filter and permissions if they came in the arguments
 				String filter = Common.getParamString(argsArray, "filter");
+				String permissions = Common.getParamString(argsArray, "permissions");
 				
 				currentSession.startTransmission();
 				currentSession.startResponse();
@@ -650,17 +651,45 @@ public class Commands
 					if (services != null)
 					{	
 						for (int i = 0; i < services.length; i++)
-						{
-							if (services[i].exported == true)
+						{							
+							boolean relevantFilter = false;
+							boolean relevantPermissions = false;
+							boolean noFilters = false;
+							boolean bothFiltersRelevant = false;
+							
+							//Check if a filter was used
+							if (filter.length() > 0)
+								relevantFilter = package_.packageName.contains(filter) || services[i].name.contains(filter);
+							
+							//Check if a permission filter was used
+							try
 							{
-								boolean filterPresent = filter.length() != 0;
-								boolean filterRelevant = package_.packageName.contains(filter) || services[i].name.contains(filter);
-								
-								if ((filterPresent && filterRelevant) || !filterPresent)
+								if (permissions.length() > 0)
 								{
-									currentSession.send("Package name: " + services[i].packageName + "\n", true);
-									currentSession.send("Service: " + services[i].name + "\n\n", true);
+									if (permissions.toUpperCase().equals("NULL"))
+										relevantPermissions = (services[i].permission == null);
+									else
+										relevantPermissions = services[i].permission.contains(permissions);
 								}
+							} catch (Throwable t) {}
+							
+							//Check if no parameters were given
+							if (filter.length() == 0 && permissions.length() == 0)
+								noFilters = true;
+							
+							boolean bothFiltersPresent = false;
+							if ((filter != "") && (permissions != ""))
+								bothFiltersPresent = true;
+							
+							if (bothFiltersPresent && relevantFilter && relevantPermissions)
+								bothFiltersRelevant = true;
+							
+							//Apply filter and only look @ exported providers
+							if (((bothFiltersPresent && bothFiltersRelevant) || (!bothFiltersPresent && (relevantFilter || relevantPermissions)) || (!bothFiltersPresent && noFilters)) && services[i].exported)
+							{
+								currentSession.send("Package name: " + services[i].packageName + "\n", true);
+								currentSession.send("Service: " + services[i].name + "\n", true);
+								currentSession.send("Required Permission: " + services[i].permission + "\n\n", true);
 							}
 						}
 					}
