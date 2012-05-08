2e550a5898ff9cff0aa6ed4c45dbee248f1c11ec
Luander Ribeiro <luander.r@samsung.com>
2012-05-08 14:21:05 -0300

    Added provider->securityscan feature

--- client/merc/lib/provider.py
+++ client/merc/lib/provider.py
@@ -288,3 +288,29 @@ usage: finduri packageName
         # FIXME: Choose specific exceptions to catch
         except:
             pass
+
+    def do_securityscan(self, args):
+        """
+Get access information about all content providers with optional filter
+usage: info [--filter <filter>]
+        """
+
+        # Define command-line arguments using argparse
+        parser = argparse.ArgumentParser(prog = 'info', add_help = False)
+        parser.add_argument('--filter', '-f', metavar = '<filter>')
+        parser.add_argument('--permissions', '-p', metavar = '<filter>')
+
+
+        try:
+
+            # Split arguments using shlex - this means that parameters with spaces can be used - escape " characters inside with \
+            splitargs = parser.parse_args(shlex.split(args))
+
+            # Compile stated arguments to send to executeCommand
+            request = vars(splitargs)
+
+            print self.session.executeCommand("provider", "securityscan", request).getPaddedErrorOrData()
+
+        # FIXME: Choose specific exceptions to catch
+        except:
+            pass
