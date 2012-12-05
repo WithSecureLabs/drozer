8c3ca7a06de6511ff931031ffe15b6598142e84b
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2012-12-05 15:03:50 +0000

    when providing path_completion, offer existing files as well as folders but allow this to be disabled

--- src/mwr/droidhg/modules/common/path_completion.py
+++ src/mwr/droidhg/modules/common/path_completion.py
@@ -1,6 +1,6 @@
 import os
 
-def on_console(path):
+def on_console(path, include_files=True):
     """
     Provides path completion, against files local to the Console.
     """
@@ -8,7 +8,7 @@ def on_console(path):
     folder, search_path = get_folder_and_search_path(path, os.path.sep)
     folders = os.listdir(folder)
     
-    return get_suggestions(folder, search_path, folders, os.path.sep)
+    return get_suggestions(folder, search_path, folders, os.path.sep, include_files)
 
 def on_agent(path):
     """
@@ -35,10 +35,15 @@ def get_folder_and_search_path(path, sep):
 
     return (folder, search_path)
 
-def get_suggestions(folder, search_path, folders, sep):
+def get_suggestions(folder, search_path, folders, sep, include_files=True):
     """
     Filter a list of folders with a given search path.
     """
 
-    return [ (folder + p + sep) for p in folders if (p.startswith(search_path) and p != search_path and os.path.isdir(folder + p)) ]
+    suggestions = [ (folder + p + sep) for p in folders if (p.startswith(search_path) and p != search_path and os.path.isdir(folder + p)) ]
+    
+    if include_files:
+        suggestions += [ (folder + p) for p in folders if (p.startswith(search_path) and p != search_path and not os.path.isdir(folder + p)) ]
+    
+    return suggestions
     
