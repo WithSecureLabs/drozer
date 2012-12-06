356aa591c632ff1d02936cd91d7cf0a7db1e3aa9
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2012-12-06 14:25:09 +0000

    adding comments throughout the Configuration class

--- src/mwr/droidhg/configuration.py
+++ src/mwr/droidhg/configuration.py
@@ -2,11 +2,19 @@ import ConfigParser
 import os
 
 class Configuration(object):
+    """
+    Configuration provides a wrapper around a user's Mercury configuration, stored
+    in their home directory as .mercury_config.
+    """
     
     __config = None
     
     @classmethod
     def delete(cls, section, key):
+        """
+        Removes a key from the configuration, and persists the change to disk.
+        """
+        
         cls.__ensure_config()
         
         cls.__config.remove_option(section, key)
@@ -14,12 +22,33 @@ class Configuration(object):
     
     @classmethod
     def get(cls, section, key):
+        """
+        Returns a single configuration value.
+        """
+        
         cls.__ensure_config()
         
         return cls.__config.get(section, key)
     
     @classmethod
+    def get_all_keys(cls, section):
+        """
+        Returns all keys from a section of the configuration file.
+        """
+        
+        cls.__ensure_config()
+        
+        if cls.__config.has_section(section):
+            return cls.__config.options(section)
+        else:
+            return []
+        
+    @classmethod
     def get_all_values(cls, section):
+        """
+        Returns all values from a section of the configuration file.
+        """
+        
         cls.__ensure_config()
         
         if cls.__config.has_section(section):
@@ -29,10 +58,19 @@ class Configuration(object):
             
     @classmethod
     def path(cls):
+        """
+        Returns the path to the configuration file.
+        """
+        
         return os.path.sep.join([os.path.expanduser("~"), ".mercury_config"])
     
     @classmethod
     def set(cls, section, key, value):
+        """
+        Sets a key in the configuration (creating it if it does not exist), and persists
+        the change to disk.
+        """
+        
         cls.__ensure_config()
         
         cls.__config.set(section, key, value)
@@ -40,6 +78,10 @@ class Configuration(object):
         
     @classmethod
     def __ensure_config(cls):
+        """
+        Loads the configuration from file, if it has not already been loaded.
+        """
+        
         if cls.__config == None:
             cls.__config = ConfigParser.SafeConfigParser()
             
