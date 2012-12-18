c6971c8a821110ededc95b3799c629924b72d312
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2012-12-18 14:25:26 +0000

    improve the test coverage of the Repository model

--- test/mwr_test/droidhg/repoman/repository_test.py
+++ test/mwr_test/droidhg/repoman/repository_test.py
@@ -1,8 +1,10 @@
 import ConfigParser
+import os
+import shutil
 import unittest
 
 from mwr.droidhg.configuration import Configuration
-from mwr.droidhg.repoman import Repository
+from mwr.droidhg.repoman.repositories import Repository, NotEmptyException, UnknownRepository
 
 class RepositoryTestCase(unittest.TestCase):
     
@@ -53,6 +55,124 @@ class RepositoryTestCase(unittest.TestCase):
         
         assert Repository.droidhg_modules_path() == ""
     
+    def testItShouldCreateAModuleRepository(self):
+        Configuration._Configuration__config = self.mockConfigWithoutRepos()
+        
+        Repository.create("./tmp")
+        
+        assert os.path.exists("./tmp")
+        assert os.path.exists("./tmp/__init__.py")
+        assert os.path.exists("./tmp/.mercury_repository")
+        
+        assert Repository.all() == ["./tmp"]
+        
+        shutil.rmtree("./tmp")
+    
+    def testItShouldNotCreateARepositoryOverAnExistingRecord(self):
+        Configuration._Configuration__config = self.mockConfigWithoutRepos()
+        
+        Repository.create("./tmp")
+        
+        try:
+            Repository.create("./tmp")
+            assert False, "expected NotEmptyException"
+        except NotEmptyException:
+            pass
+        
+        shutil.rmtree("./tmp")
+    
+    def testItShouldNotCreateARepositoryOverAnExistingFolder(self):
+        Configuration._Configuration__config = self.mockConfigWithoutRepos()
+        
+        os.mkdir("./tmp")
+        
+        try:
+            Repository.create("./tmp")
+            assert False, "expected NotEmptyException"
+        except NotEmptyException:
+            pass
+        
+        shutil.rmtree("./tmp")
+    
+    def testItShouldDeleteAModuleRepository(self):
+        Configuration._Configuration__config = self.mockConfigWithoutRepos()
+        
+        Repository.create("./tmp")
+        Repository.delete("./tmp")
+        
+        assert not os.path.exists("./tmp")
+        
+        assert Repository.all() == []
+    
+    def testItShouldNotDeleteAnUnknownRepository(self):
+        Configuration._Configuration__config = self.mockConfigWithoutRepos()
+        
+        os.mkdir("./tmp")
+        
+        try:
+            Repository.delete("./tmp")
+            
+            assert False, "expected UnknownRepository"
+        except UnknownRepository:
+            pass
+        
+        assert os.path.exists("./tmp")
+        
+        shutil.rmtree("./tmp")
+    
+    def testItShouldEnableAModuleRepository(self):
+        Configuration._Configuration__config = self.mockConfigWithoutRepos()
+        
+        Repository.create("./tmp")
+        
+        Configuration._Configuration__config = self.mockConfigWithoutRepos()
+        
+        Repository.enable("./tmp")
+        
+        assert Repository.all() == ["./tmp"]
+        
+        shutil.rmtree("./tmp")
+    
+    def testItShouldNotEnableAFolderThatIsnNotAModuleRepository(self):
+        Configuration._Configuration__config = self.mockConfigWithoutRepos()
+        
+        os.mkdir("./tmp")
+        
+        try:
+            Repository.enable("./tmp")
+            
+            assert False, "expected UnknownRepository"
+        except UnknownRepository:
+            pass
+        
+        assert Repository.all() == []
+        
+        shutil.rmtree("./tmp")
+    
+    def testItShouldDisableAModuleRepository(self):
+        Configuration._Configuration__config = self.mockConfigWithRepos([])
+        
+        Repository.create("./tmp")
+        Repository.disable("./tmp")
+        
+        assert os.path.exists("./tmp")
+        assert os.path.exists("./tmp/__init__.py")
+        assert os.path.exists("./tmp/.mercury_repository")
+        
+        assert Repository.all() == []
+        
+        shutil.rmtree("./tmp")
+    
+    def testItShouldNotDisableAnUnknownModuleRepository(self):
+        Configuration._Configuration__config = self.mockConfigWithRepos([])
+        
+        try:
+            Repository.disable("./tmp")
+            
+            assert False, "expected UnknownRepository"
+        except UnknownRepository:
+            pass
+    
 def RepositoryTestSuite():
     suite = unittest.TestSuite()
 
@@ -63,6 +183,15 @@ def RepositoryTestSuite():
     suite.addTest(RepositoryTestCase("testItShouldBuildDroidhgModulesPathWithAnAdditionalRepository"))
     suite.addTest(RepositoryTestCase("testItShouldBuildDroidhgModulesPathWithTwoAdditionalRepository"))
     suite.addTest(RepositoryTestCase("testItShouldBuildDroidhgModulesPathWithoutAConfigFile"))
+    suite.addTest(RepositoryTestCase("testItShouldCreateAModuleRepository"))
+    suite.addTest(RepositoryTestCase("testItShouldNotCreateARepositoryOverAnExistingRecord"))
+    suite.addTest(RepositoryTestCase("testItShouldNotCreateARepositoryOverAnExistingFolder"))
+    suite.addTest(RepositoryTestCase("testItShouldDeleteAModuleRepository"))
+    suite.addTest(RepositoryTestCase("testItShouldNotDeleteAnUnknownRepository"))
+    suite.addTest(RepositoryTestCase("testItShouldEnableAModuleRepository"))
+    suite.addTest(RepositoryTestCase("testItShouldNotEnableAFolderThatIsnNotAModuleRepository"))
+    suite.addTest(RepositoryTestCase("testItShouldDisableAModuleRepository"))
+    suite.addTest(RepositoryTestCase("testItShouldNotDisableAnUnknownModuleRepository"))
 
     return suite
   
