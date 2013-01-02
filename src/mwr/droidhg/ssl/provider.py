79727e349f0bdcc1ef0f7862337eab6497b74d7e
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2013-01-02 15:49:24 +0000

    adding an SSLManager, which uses the Provider to provision key material

---
+++ src/mwr/droidhg/ssl/provider.py
@@ -0,0 +1,161 @@
+import os
+import OpenSSL
+import random
+import shlex
+import string
+
+from mwr.common import fs, system
+
+from mwr.droidhg.configuration import Configuration
+from mwr.droidhg.ssl import ca
+
+class Provider(object):
+    
+    def __init__(self):
+        self.authority = ca.CA()
+    
+    def ca_certificate_path(self):
+        """
+        Get the path to the CA Certificate.
+        """
+        
+        return self.__certificate_path("mercury-ca")
+    
+    def ca_path(self):
+        """
+        Get the path to the CA Key Material, as defined by the configuration file.
+        """
+        
+        return Configuration.get("ssl", "ca_path")
+
+    def certificate_exists(self):
+        """
+        True, if the CA certificate file exists, and can be read.
+        """
+        
+        return os.path.exists(self.ca_certificate_path())
+    
+    def create_keypair(self, cn):
+        """
+        Create a key/certificate pair, signed with the CA.
+        """
+        
+        self.__load_key_material()
+        
+        return self.authority.create_certificate(cn)
+    
+    def key_exists(self):
+        """
+        True, if the CA key file exists, and can be read.
+        """
+        
+        return os.path.exists(self.__ca_key_path())
+    
+    def key_material_exists(self):
+        """
+        True, if the CA Key Material exists.
+        """
+        
+        return self.key_exists() and self.certificate_exists()
+    
+    def key_material_valid(self):
+        """
+        Tests the given Key Material, to determine if it is a valid key pair.
+        """
+        
+        self.__load_key_material()
+        
+        return self.authority.verify_ca()
+    
+    def make_bks(self, cn, p12_path, export_password, store_password, key_password):
+        """
+        Prepare a BouncyCastle KeyStore from a PKCS12 bundle.
+        """
+        
+        keytool = system.which('keytool') 
+        argv = shlex.split(("%s " +
+                            "-importkeystore "+
+                            "-deststorepass %s "+
+                            "-destkeypass %s " +
+                            "-destkeystore %s " +
+                            "-deststoretype BKS " + 
+                            "-provider org.bouncycastle.jce.provider.BouncyCastleProvider " +
+                            "-providerpath /usr/local/share/classpath/bcprov-ext-jdk15on-1.46.jar "
+                            "-srckeystore %s " +
+                            "-srcstoretype PKCS12 " +
+                            "-srcstorepass %s " +
+                            "-alias mercury") % (keytool, store_password, key_password, self.__bks_path(cn), p12_path, export_password))
+        
+        if os.spawnvpe(os.P_WAIT, argv[0], argv, os.environ) == 0:
+            return self.__bks_path(cn)
+    
+    def make_pcks12(self, cn, key, cert, export_password=None):
+        """
+        Prepare a PKCS12 package, given a key and a certificate.
+        """
+        
+        if export_password == None:
+            export_password = ''.join(random.choice(string.ascii_letters + string.digits) for x in range(12))
+        
+        pkcs12 = OpenSSL.crypto.PKCS12()
+        pkcs12.set_friendlyname("mercury")
+        pkcs12.set_ca_certificates([self.authority.ca_cert])
+        pkcs12.set_certificate(cert)
+        pkcs12.set_privatekey(key)
+        
+        fs.write(os.path.join(self.ca_path(), "%s.p12" % cn), pkcs12.export(export_password))
+        
+        return (os.path.join(self.ca_path(), "%s.p12" % cn), export_password)
+        
+    def provision(self, path):
+        """
+        Provision new CA Key Material.
+        
+        This will overwrite any existing CA.
+        """
+        
+        self.authority.create_ca()
+        
+        Configuration.set("ssl", "ca_path", path)
+        
+        fs.write(self.ca_certificate_path(), ca.CA.certificate_to_pem(self.authority.ca_cert))
+        fs.write(self.__ca_key_path(), ca.CA.pkey_to_pem(self.authority.ca_key))
+        
+        return True
+    
+    def __bks_path(self, cn):
+        """
+        Get the path to a BouncyCastle KeyStore file.
+        """
+        
+        return os.path.join(self.ca_path(), "%s.bks" % cn)
+        
+    def __ca_key_path(self):
+        """
+        Get the path to the CA Key.
+        """
+        
+        return self.__key_path("mercury-ca")
+    
+    def __certificate_path(self, cn):
+        """
+        Get the path to a certificate file.
+        """
+        
+        return os.path.join(self.ca_path(), "%s.crt" % cn)
+    
+    def __key_path(self, cn):
+        """
+        Get the path to a key file.
+        """
+        
+        return os.path.join(self.ca_path(), "%s.key" % cn)
+        
+    def __load_key_material(self):
+        """
+        Loads Key Material into the authority, from disk.
+        """
+        
+        if not self.authority.has_ca() and self.key_material_exists():
+            self.authority.load(fs.read(self.__ca_key_path()), fs.read(self.ca_certificate_path()))
+            
--- src/mwr/droidhg/ssl/ssl_manager.py
+++ src/mwr/droidhg/ssl/ssl_manager.py
@@ -1,4 +1,7 @@
-from mwr.common import cli
+import os
+
+from mwr.common import cli, fs
+from mwr.droidhg.ssl.provider import Provider
 
 class SSLManager(cli.Base):
     """
@@ -12,10 +15,50 @@ class SSLManager(cli.Base):
     def __init__(self):
         cli.Base.__init__(self)
         
-        self._parser.add_argument("type", choices=["ca", "keypair"], help="the type of key material to create")
+        self._parser.add_argument("type", choices=["ca", "keypair"], help="the type of key material to create", nargs='?')
+        self._parser.add_argument("subject", help="the subject CN, when generating a keypair", nargs='?')
     
     def do_create(self, arguments):
         """create some new key material"""
         
-        print "creating", arguments.type
+        provider = Provider()
+        
+        if arguments.type == "ca":
+            path = provider.ca_path()
+            
+            if path == None or not os.path.exists(path):
+                path = os.path.abspath(os.curdir)
+            
+            if provider.provision(path):
+                print "Created Authority."
+            else:
+                print "There was a problem whilst creating the authority."
+        elif arguments.type == "keypair":
+            if arguments.subject == None or arguments.subject == "":
+                print "Please specify the subject CN."
+            else:
+                key, certificate = provider.create_keypair(arguments.subject)
+                
+                p12_path, export_password = provider.make_pcks12(arguments.subject, key, certificate)
+                bks_path = provider.make_bks(arguments.subject, p12_path, export_password, "mercury", "mercury")
+                
+                if bks_path != None:
+                    print "Created SSL keypair, %s: %s" % (arguments.subject, bks_path)
+                else:
+                    print "There was a problem creating the BKS KeyStore."
+        else:
+            print "Unexpected type:", arguments.type
+    
+    def do_show(self, arguments):
+        """show SSL configuration information"""
+        
+        provider = Provider()
+        
+        if provider.key_material_exists():
+            print "SSL has been provisioned:"
+            print "          SSL Key:", provider.key_exists() and "EXISTS" or "MISSING"
+            print "  SSL Certificate:", provider.certificate_exists() and "EXISTS" or "MISSING"
+            print "         Key Pair:", provider.key_material_valid() and "VALID" or "INVALID"
+        else:
+            print "SSL has not been provisioned."
         
