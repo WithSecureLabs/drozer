cb81ad28a934a6863a1f9c8a309b8d7e75c63ba1
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2013-01-11 09:16:17 +0000

    fixing a bug in console.format_dict, where passing it an empty dictionary would cause an error

--- src/mwr/common/console.py
+++ src/mwr/common/console.py
@@ -10,7 +10,7 @@ from mwr.common import text
 
 def format_dict(values, left_margin=0):
     width = { 'gutter': 2, 'left_margin': left_margin, 'total': get_size()[0] - left_margin }
-    width['key'] = min([max(map(lambda k: len(k), values.keys())), width['total'] / 3])
+    width['key'] = min([max(map(lambda k: len(k), values.keys()) + [0]), width['total'] / 3])
     width['value'] = width['total'] - (width['gutter'] + width['key'])
     
     template_key_only = "%%%ds%%-%ds\n" % (width['left_margin'], width['key']) 
