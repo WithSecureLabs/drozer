05b5acbc62bbcba96295a84fcfe916a623a56d4a
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2013-05-03 09:43:35 +0100

    adding a meta module to keep track of application information, such as name, vendor and version

---
+++ src/mwr/mercury/meta.py
@@ -0,0 +1,14 @@
+
+name = "mercury"
+vendor = "MWR InfoSecurity"
+version = "2.3.0"
+
+contact = "mercury@mwrinfosecurity.com"
+description = "Android Security Testing Framework"
+license = "MWR Code License v2"
+keywords = "android security framework"
+url = "http://mwr.to/mercury"
+
+def print_version():
+    print "%s %s\n" % (name, version)
+    
