f03234512381adae4005b63715d8441dcfc354ac
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2013-05-22 10:53:39 +0100

    fixing up an incorrect import in system_request_handler

--- src/drozer/api/handlers/system_request_handler.py
+++ src/drozer/api/handlers/system_request_handler.py
@@ -7,7 +7,8 @@ from pydiesel.api import handlers
 from pydiesel.api.builders import SystemResponseFactory
 from pydiesel.api.protobuf_pb2 import Message
 
-from drozer.session import Devices, Sessions
+from drozer.device import Devices
+from drozer.session import Sessions
 
 class SystemRequestHandler(handlers.SystemRequestHandler):
     """
