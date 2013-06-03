37d8001d2fbc785a25e770d471e4de4865f55aa3
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2013-06-03 17:06:01 +0100

    implement a subset of the HTTP Protocol in the server; we can GET, POST and DELETE a resource - optionally preventing overwriting of existing resources

--- src/drozer/server/http.py
+++ src/drozer/server/http.py
@@ -1,6 +1,7 @@
 from logging import getLogger
 from twisted.internet.protocol import Protocol
 
+from drozer.server.files import FileProvider, ErrorResource
 from drozer.server.receivers.http import HttpReceiver, HTTPResponse
 
 class HTTP(HttpReceiver):
@@ -28,6 +29,30 @@ class HTTP(HttpReceiver):
         Called when a complete HTTP request has been made to the HTTP server.
         """
         
-        self.transport.write(str(HTTPResponse(body=request.resource)))
+        resource = None
+        
+        if request.verb == "DELETE":
+            resource = self.__file_provider.get(request.resource)
+            
+            if resource != None and resource.reserved:
+                resource = ErrorResource(request.resource, 403, "You are not authorized to delete the resource %s.")
+            else:
+                self.__file_provider.delete(request.resource)
+                
+                resource = ErrorResource(request.resource, 200, "Deleted: %s")
+        elif request.verb == "GET":
+            resource = self.__file_provider.get(request.resource)
+        elif request.verb == "POST":
+            resource = self.__file_provider.get(request.resource)
+            
+            if resource != None and resource.reserved:
+                resource = ErrorResource(request.resource, 403, "You are not authorized to write the resource %s.")
+            else:
+                if self.__file_provider.create(request.resource, request.body):
+                    resource = ErrorResource(request.resource, 201, "Location: %s")
+                else:
+                    resource = ErrorResource(request.resource, 500, "The server encountered an error whilst creating the resource %s.")
+        
+        self.transport.write(str(resource.getResponse()))
         self.transport.loseConnection()
         
