fa44f92df7ff96da5d19c9a5bfc432b587b7a41b
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2013-06-03 16:08:41 +0100

    adding an implementation of an HTTP server that reads a request, and responds with the requested resource name

--- src/drozer/server/receivers/http.py
+++ src/drozer/server/receivers/http.py
@@ -153,7 +153,7 @@ class HTTPRequest(HTTPMessage):
 
 class HTTPResponse(HTTPMessage):
     
-    def __init__(self, status=200, version="HTTP/1.1", headers=None, body=None):
+    def __init__(self, status=200, version="HTTP/1.1", headers=None, body=""):
         HTTPMessage.__init__(self, headers, body)
         
         self.status = status
@@ -164,7 +164,47 @@ class HTTPResponse(HTTPMessage):
         return "\r\n".join(map(lambda k: "%s: %s" % (k,self.headers[k]), self.headers))
     
     def status_text(self):
-        return { 200: "OK" }[self.status]
+        return { 100: "Continue",
+                 101: "Switching Protocols",
+                 200: "OK",
+                 201: "Created",
+                 202: "Accepted",
+                 203: "Non-Authoritative Information",
+                 204: "No Content",
+                 205: "Reset Content",
+                 206: "Partial Content",
+                 300: "Multiple Choices",
+                 301: "Moved Permanently",
+                 302: "Found",
+                 303: "See Other",
+                 304: "Not Modified",
+                 305: "Use Proxy",
+                 307: "Temporary Redirect",
+                 400: "Bad Request",
+                 401: "Unauthorized",
+                 402: "Payment Required",
+                 403: "Forbidden",
+                 404: "Not Found",
+                 405: "Method Not Allowed",
+                 406: "Not Acceptable",
+                 407: "Proxy Authentication Required",
+                 408: "Request Timeout",
+                 409: "Conflict",
+                 410: "Gone",
+                 411: "Length Required",
+                 412: "Precondition Failed",
+                 413: "Request Entity Too Large",
+                 414: "Request-URI Too Long",
+                 415: "Unsupported Media Type",
+                 416: "Requested Range Not Satisfiable",
+                 417: "Expectation Failed",
+                 418: "I'm a teapot",
+                 500: "Internal Server Error",
+                 501: "Not Implemented",
+                 502: "Bad Gateway",
+                 503: "Service Unavailable",
+                 504: "Gateway Timeout",
+                 505: "HTTP Version Not Supported" }[self.status]
     
     def __str__(self):
         return "%s %d %s\r\n%s\r\n\r\n%s" % (self.version, self.status, self.status_text(), self.format_headers(), self.body)
