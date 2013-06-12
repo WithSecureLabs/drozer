c9a0e182f70c9521af1e420040ec400660c9ae3e
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2013-06-12 13:48:24 +0100

    adding the glue that shows us available shellcode modules, and executes them to dump shellcode to the screen

---
+++ src/drozer/shellcode/manager.py
@@ -0,0 +1,39 @@
+from mwr.common import cli
+
+from drozer.shellcode import builder
+
+class ShellCodeManager(cli.Base):
+    """
+    drozer shellcode COMMAND [OPTIONS]
+    
+    A utility for crafting shellcode to deliver drozer onto an Android device.
+    """
+    
+    def __init__(self):
+        cli.Base.__init__(self)
+    
+    def args_for_build(self):
+        self._parser.add_argument("module", help="specify the shellcode module to use")
+        self._parser.add_argument("--format", choices=builder.Formats, default=builder.Formats[0], help="specify the format for the shellcode (Raw bytes, Unicode or heX string)")
+        self._parser.add_argument("--server", default=None, metavar="HOST[:PORT]", help="specify the address and port of the drozer server")
+        
+    def do_build(self, arguments):
+        """build some shellcode"""
+        
+        print builder.Builder().build(arguments.module, arguments)
+
+    def args_for_info(self):
+        self._parser.add_argument("module", help="specify the shellcode module to use")
+        
+    def do_info(self, arguments):
+        """prints information about a shellcode module"""
+        
+        module = builder.Builder().module(arguments.module)
+        
+        print module.usage.formatted_description()
+        
+    def do_list(self, arguments):
+        """list the available shellcode modules"""
+        
+        print builder.Builder().modules()
+        
