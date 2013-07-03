7739be9aca0dcd72af7bb43caa96b4c7b503c085
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2013-07-03 08:23:12 +0100

    replacing the uploader with a synchronous implementation that doesn't fail if we try to upload two files in one process

--- src/drozer/server/uploader.py
+++ src/drozer/server/uploader.py
@@ -1,63 +1,47 @@
 from base64 import b64encode
-
-from twisted.internet import ssl, reactor
-from twisted.internet.protocol import ClientFactory, Protocol
+from socket import socket
+import ssl
 
 from drozer.server.receivers.http import HTTPRequest, HTTPResponse
+from drozer.ssl.provider import Provider
 
 def delete(arguments, resource):
-    factory = UploaderFactory(arguments, "DELETE", resource, "", None)
-    if arguments.ssl != None:
-        reactor.connectSSL(arguments.server[0], arguments.server[1], factory, ssl.DefaultOpenSSLContextFactory(*arguments.ssl))
-    else:
-        reactor.connectTCP(arguments.server[0], arguments.server[1], factory)
-    reactor.run()
+    pass
+    #factory = UploaderFactory(arguments, "DELETE", resource, "", None)
+    #if arguments.ssl != None:
+    #    reactor.connectSSL(arguments.server[0], arguments.server[1], factory, ssl.DefaultOpenSSLContextFactory(*arguments.ssl))
+    #else:
+    #    reactor.connectTCP(arguments.server[0], arguments.server[1], factory)
+    #reactor.run()
 
 def upload(arguments, resource, data, magic=None):
-    factory = UploaderFactory(arguments, "POST", resource, data, magic)
-    if arguments.ssl != None:
-        reactor.connectSSL(arguments.server[0], arguments.server[1], factory, ssl.DefaultOpenSSLContextFactory(*arguments.ssl))
-    else:
-        reactor.connectTCP(arguments.server[0], arguments.server[1], factory)
-    reactor.run()
-
-class Uploader(Protocol):
+    #factory = UploaderFactory(arguments, "POST", resource, data, magic)
+    sock = socket()
     
-    def connectionMade(self):
-        request = HTTPRequest(verb=self.factory.verb, resource=self.factory.resource, body=self.factory.data)
-        if self.factory.arguments.credentials != None:
-            request.headers["Authorization"] = "Basic %s" % b64encode(":".join(self.factory.arguments.credentials))
-        request.headers["Content-Length"] = len(self.factory.data)
-        if self.factory.magic != None:
-            request.headers["X-Drozer-Magic"] = self.factory.magic
+    if arguments.ssl:
+        provider = Provider()
         
-        self.transport.write(str(request))
+        sock = ssl.wrap_socket(sock, cert_reqs=ssl.CERT_REQUIRED, ca_certs=provider.ca_certificate_path())
 
-    def dataReceived(self, data):
-        response = HTTPResponse.parse(data)
-        
-        if response.status == 201:
-            print "Successfully Uploaded %s." % response.headers["Location"]
-        else:
-            print response.headers
-            print response.body
-            
-        self.transport.loseConnection()
-                
-class UploaderFactory(ClientFactory):
+    sock.settimeout(90.0)
+    sock.connect(arguments.server)
     
-    protocol = Uploader
-
-    def __init__(self, arguments, verb, resource, data, magic=None):
-        self.arguments = arguments
-        self.data = data
-        self.magic = magic
-        self.resource = resource
-        self.verb = verb
+    #if arguments.ssl:
+        #trust_callback(provider, sock.getpeercert(True), sock.getpeername())
+    
+    request = HTTPRequest(verb="POST", resource=resource, body=data)
+    if arguments.credentials != None:
+        request.headers["Authorization"] = "Basic %s" % b64encode(":".join(arguments.credentials))
+    request.headers["Content-Length"] = len(data)
+    if magic != None:
+        request.headers["X-Drozer-Magic"] = magic
         
-    def clientConnectionFailed(self, connector, reason):
-        reactor.stop()
+    request_data = str(request)
+    sent = 0
+    
+    while sent < len(request_data):
+        sent += sock.send(request_data[sent:])
+    
+    response = HTTPResponse.readFrom(sock)
 
-    def clientConnectionLost(self, connector, reason):
-        reactor.stop()
-        
+    return response.status == 201
