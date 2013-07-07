c30235a994617dcf66e3d685d2ce7cfab15a5f33
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2013-07-07 09:20:40 +0100

    breaking out some of the common exploit generation into an exploit module

--- src/drozer/modules/common/__init__.py
+++ src/drozer/modules/common/__init__.py
@@ -7,6 +7,7 @@ package manager and some templates for modules.
 from drozer.modules.common.assets import Assets
 from drozer.modules.common.binding import ServiceBinding
 from drozer.modules.common.busy_box import BusyBox
+from drozer.modules.common.exploit import Exploit
 from drozer.modules.common.file_system import FileSystem
 from drozer.modules.common.filtering import Filters
 from drozer.modules.common.formatter import TableFormatter
