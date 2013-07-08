f106245f461708f657f687b7d7f4cd31b1e364a6
Daniel Bradberry <daniel.bradberry@mwrinfosecurity.com>
2013-07-08 12:35:57 +0100

    add an optional --permission flag when building an exploit, to specify the additional permissions that we should ask for in the manifest

--- src/drozer/payload/manager.py
+++ src/drozer/payload/manager.py
@@ -13,7 +13,7 @@ class PayloadManager(cli.Base):
         cli.Base.__init__(self)
         
         self._parser.add_argument("--no-gui", action="store_true", default=False, help="create an agent with no GUI")
-        self._parser.add_argument("--permission", "-p", nargs="+", help="add a permission to the Agent manifest")
+        self._parser.add_argument("--permission", "-p", nargs="+", help="add permissions to the Agent manifest")
         self._parser.add_argument("--server", default=None, metavar="HOST[:PORT]", help="specify the address and port of the drozer server")
         
     def do_build(self, arguments):
