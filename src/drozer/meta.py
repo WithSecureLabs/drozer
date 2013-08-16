33a773fa722a8e56e1904d0589160ca5e6d55b48
Daniel Bradberry <user.email>
2013-08-16 20:02:12 +0100

    we are aiming towards version 2.3.1

--- src/drozer/meta.py
+++ src/drozer/meta.py
@@ -1,7 +1,7 @@
 
 name = "drozer"
 vendor = "MWR InfoSecurity"
-version = "2.3.0"
+version = "2.3.1"
 
 contact = "drozer@mwrinfosecurity.com"
 description = "The Leading Android Security Testing Framework"
