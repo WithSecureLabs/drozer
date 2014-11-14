53c04a1ce1fc380485e466c4549b5585ac78b053
metall0id <tyrone.erasmus@gmail.com>
2014-11-14 23:22:27 +0200

    Deprecated --no-gui for the more descriptive --rogue. --no-gui will continue to work though

--- src/drozer/agent/manager.py
+++ src/drozer/agent/manager.py
@@ -14,7 +14,8 @@ class AgentManager(cli.Base):
     def __init__(self):
         cli.Base.__init__(self)
         
-        self._parser.add_argument("--no-gui", action="store_true", default=False, help="create an agent with no GUI")
+        self._parser.add_argument("--rogue", action="store_true", default=False, help="create a rogue agent with no GUI")
+        self._parser.add_argument("--no-gui", action="store_true", default=False, help="deprecated: rather use --rogue. create an agent with no GUI")
         self._parser.add_argument("--granular", action="store_true", default=False, help="don't request all permissions when building GUI-less agent")
         self._parser.add_argument("--permission", "-p", nargs="+", help="add permissions to the Agent manifest")
         self._parser.add_argument("--define-permission", "-d", metavar="name protectionLevel", nargs="+", help="define a permission and protectionLevel in the Agent manifest")
@@ -23,11 +24,11 @@ class AgentManager(cli.Base):
     def do_build(self, arguments):
         """build a drozer Agent"""
 
-        source = arguments.no_gui and "rogue-agent" or "standard-agent"
+        source = (arguments.rogue or arguments.no_gui) and "rogue-agent" or "standard-agent"
         packager = builder.Packager()
         packager.unpack(source)
         
-        if arguments.no_gui:
+        if arguments.rogue or arguments.no_gui:
             e = manifest.Endpoint(packager.endpoint_path())
             if arguments.server != None:
                 e.put_server(arguments.server)
